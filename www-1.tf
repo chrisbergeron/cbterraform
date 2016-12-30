22437ef2a3848f18747226e2b0144eb87880df8c
Chris Bergeron <cbergeron@ionicsecurity.com>
2016-12-30 18:13:36 -0500

    New commit

--- www-1.tf
+++ www-1.tf
@@ -22,7 +22,16 @@ provisioner "remote-exec" {
       "sudo systemctl start nginx",
       "sudo wget https://github.com/digitalocean/doctl/releases/download/v1.5.0/doctl-1.5.0-linux-amd64.tar.gz",
       "sudo tar xvfz doctl-1.5.0-linux-amd64.tar.gz",
-      "sudo mv doctl /usr/local/bin"
+      "sudo mv doctl /usr/local/bin",
+      "sudo rm -f doctl-1.5.0-linux-amd64.tar.gz",
+      "sudo yum -y install mariadb-server mariadb",
+      "sudo systemctl start mariadb",
+      "sudo mysql -e \"UPDATE mysql.user SET Password=PASSWORD('test') WHERE User='root';\"",
+      "sudo mysql -e \"DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');\"",
+      "sudo mysql -e \"DELETE FROM mysql.user WHERE User='';\"",
+      "sudo mysql -e \"DROP DATABASE test;\"",
+      "sudo mysql -e \"FLUSH PRIVILEGES;\"",
+      "sudo systemctl enable mariadb"
     ]
   }
 }
