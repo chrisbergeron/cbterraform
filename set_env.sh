5634aa5c01c62002f7ccc59f7f1448ad8be04d6c
Chris Bergeron <cbergeron@ionicsecurity.com>
2017-01-11 13:12:21 -0500

    Removed set_env.sh.

--- set_env.sh
+++ set_env.sh
@@ -3,5 +3,8 @@
 export DO_PAT=`cat ~/Documents/mystuff/do_api_key.txt`
 export SSH_FINGERPRINT=`ssh-keygen -E md5 -lf ~/.ssh/id_rsa | awk '{ print $2 }' | sed -e 's/MD5://g'`
 
-set | grep DO_PAT
-set | grep FINGER
+export TF_VAR_do_token=`cat ~/Documents/mystuff/do_api_key.txt`
+export TF_VAR_ssh_fingerprint=`ssh-keygen -E md5 -lf ~/.ssh/id_rsa | awk '{ print $2 }' | sed -e 's/MD5://g'`
+
+set | grep TF_VAR_do_token
+set | grep TF_VAR_ssh_fingerprint
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,5 @@
 do_token = "626d2809762a6c65971a02ca02e2363dcdbd99786a1bdb40553fa2d692cab576"
+#do_token = "${var.do_token}"
 ssh_fingerprint = "85:02:0a:2e:05:af:d2:bc:79:90:27:dc:a8:bc:a8:e0"
 pub_key = "/Users/cbergeron/.ssh/id_rsa.pub"
 pvt_key = "/Users/cbergeron/.ssh/id_rsa_decrypted"
